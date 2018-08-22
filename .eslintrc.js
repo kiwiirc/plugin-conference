e76008f0a4818fc38b19019829fe4935d256610b
ItsOnlyBinary <github@itsonlybinary.com>
2018-08-23 00:48:14 +0100

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module'
+    },
+    extends: [
+        'airbnb-base',
+        'standard'
+    ],
+    env: {
+        'browser': true,
+    },
+    // add your custom rules here
+    'rules': {
+        'class-methods-use-this': 0,
+        'comma-dangle': ['error', {
+            'arrays': 'always-multiline',
+            'objects': 'always-multiline',
+            'imports': 'never',
+            'exports': 'never',
+            'functions': 'ignore'
+        }],
+        'import/extensions': 0,
+        'import/no-extraneous-dependencies': 0,
+        'import/no-unresolved': 0,
+        'import/prefer-default-export': 0,
+        'indent': ['error', 2],
+        'no-continue': 0,
+        'no-multi-assign': 0,
+        'no-param-reassign': ['error', { 'props': false }],
+        'no-plusplus': 0,
+        'no-prototype-builtins': 0,
+        'prefer-promise-reject-errors': 0,
+        'no-control-regex': 0,
+        'object-shorthand': 0,
+        'operator-linebreak': 0,
+        'prefer-const': 0,
+        'prefer-destructuring': 0,
+        'prefer-template': 0,
+        'semi': ['error', 'always'],
+        'space-before-function-paren': ['error', 'never'],
+        'vue/html-indent': ['error', 4],
+        'vue/max-attributes-per-line': 0,
+    }
+}
