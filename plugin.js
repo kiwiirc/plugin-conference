3e16175072b85349a33bac3b0f4c8a9ed68fd80b
Scott McGann <s.r.mcgann@hotmail.com>
2018-08-23 09:31:01 -0700

    fixed all linting errors

--- plugin.js
+++ plugin.js
@@ -1,40 +1,41 @@
-kiwi.plugin('conferencePlugin', (kiwi, log) => {
-  let api = token = null;
+/* eslint-disable vue/html-indent */
+kiwi.plugin('conferencePlugin', (kiwi, log) => { /* eslint-disable-line no-undef */
+  let api = null;
+  let token = null;
   // captionTimer holds a per-buffer value to control grouping of join messages
   let captionTimer = [];
   // captions holds the actual message data that is displayed upon conference joins
   let captions = [];
   let kiwiConferenceTag = 1;
-  let jitsiDomain = kiwi.state.setting('conference.server') || 'meet.jit.si'
-  let jitsiApiUrl = kiwi.state.setting('conference.jitsiApiUrl') || 'https://' + jitsiDomain + '/external_api.min.js'
+  let jitsiDomain = kiwi.state.setting('conference.server') || 'meet.jit.si';
+  let jitsiApiUrl = kiwi.state.setting('conference.jitsiApiUrl') || 'https://' + jitsiDomain + '/external_api.min.js';
   const groupedNoticesTTL = 30000;
 
   // Load any jitsi UI config settings
-  let interfaceConfigOverwriteFromConfig = kiwi.state.setting('conference.interfaceConfigOverwrite') || {}
+  let interfaceConfigOverwriteFromConfig = kiwi.state.setting('conference.interfaceConfigOverwrite') || {};
   let interfaceConfigOverwrite = {
-    "SHOW_JITSI_WATERMARK": false,
-    "SHOW_WATERMARK_FOR_GUESTS": false,
-    "TOOLBAR_BUTTONS": [
-      "microphone", "camera", "fullscreen", "fodeviceselection", "hangup",
-      "settings", "videoquality", "filmstrip",
-      "stats", "shortcuts"
-    ]
-  }
-  Object.keys(interfaceConfigOverwriteFromConfig).forEach(key => {
+    SHOW_JITSI_WATERMARK: false,
+    SHOW_WATERMARK_FOR_GUESTS: false,
+    TOOLBAR_BUTTONS: [
+      'microphone', 'camera', 'fullscreen', 'fodeviceselection', 'hangup',
+      'settings', 'videoquality', 'filmstrip',
+      'stats', 'shortcuts',
+    ],
+  };
+  Object.keys(interfaceConfigOverwriteFromConfig).forEach((key) => {
     interfaceConfigOverwrite[key] = interfaceConfigOverwriteFromConfig[key];
   });
 
   // Load any jitsi general config settings
-  let configOverwriteFromConfig = kiwi.state.setting('conference.configOverwrite') || {}
+  let configOverwriteFromConfig = kiwi.state.setting('conference.configOverwrite') || {};
   let configOverwrite = {
-    "startWithVideoMuted": true,
-    "startWithAudioMuted": true
-  }
-  Object.keys(configOverwriteFromConfig).forEach(key => {
+    startWithVideoMuted: true,
+    startWithAudioMuted: true,
+  };
+  Object.keys(configOverwriteFromConfig).forEach((key) => {
     configOverwrite[key] = configOverwriteFromConfig[key];
   });
-  
-  
+
   // Add the call button to the channel+query headers
   const conferencingTool = document.createElement('div');
   conferencingTool.style.marginLeft = '10px';
@@ -42,18 +43,18 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
   conferencingTool.innerHTML = '<i aria-hidden="true" class="fa fa-phone"></i>';
   kiwi.addUi('header_channel', conferencingTool);
   kiwi.addUi('header_query', conferencingTool);
-  conferencingTool.onclick = function(e){
+  conferencingTool.onclick = (e) => {
     e.preventDefault();
-    if(api){
+    if (api) {
       hideCams();
-    }else{
+    } else {
       showCams();
     }
-  }
+  };
 
   // The component that gets shown in the messagelist when somebody joins a conference call
   const joinCallMessageComponent = kiwi.Vue.extend({
-    template:`<div style="width:100%; padding: 20px; background: #123; text-align: center; color: #ffe; font-size: 2em; line-height: 1.1em;">
+    template: `<div style="width:100%; padding: 20px; background: #123; text-align: center; color: #ffe; font-size: 2em; line-height: 1.1em;">
       <div v-for="(caption, idx) in captions" :key="caption">
         {{caption}}<br>
         <span style="font-size:.6em;" v-if="idx === captions.length-1 && caption.indexOf('is inviting you to a private call.') === -1">the above users have joined the conference.</span>
@@ -72,15 +73,15 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
     },
   });
 
-  kiwi.on('message.new', function (newMessage, buffer) {
+  kiwi.on('message.new', (newMessage, buffer) => {
     let messageTemplate = null;
     let message = '';
     let nick = '';
     if (newMessage.tags && newMessage.tags['+kiwiirc.com/conference'] === kiwiConferenceTag) {
       nick = newMessage.nick;
-      if(buffer.isChannel()) {
+      if (buffer.isChannel()) {
         let bufferMessages = buffer.getMessages();
-        for(let i = bufferMessages.length; i--; ) {
+        for (let i = bufferMessages.length; i--;) {
           if (bufferMessages[i].tags && bufferMessages[i].tags['+kiwiirc.com/conference'] === kiwiConferenceTag) {
             messageTemplate = bufferMessages[i];
             break;
@@ -88,35 +89,38 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
         }
       }
       let timerKey = window.kiwi.state.getActiveNetwork().name + buffer.name;
-      // if this is the first join message in groupedNoticesTTL milliseconds, or is a private message, inject a new in-call component
-      if (typeof captionTimer[timerKey] === 'undefined' || Date.now() - captionTimer[timerKey] > groupedNoticesTTL || buffer.isQuery()){
+      // if this is the first join message in groupedNoticesTTL milliseconds,
+      // or is a private message, inject a new in-call component
+      if (typeof captionTimer[timerKey] === 'undefined' || Date.now() - captionTimer[timerKey] > groupedNoticesTTL || buffer.isQuery()) {
         messageTemplate = newMessage;
         captions[timerKey] = [];
       } else {
-        newMessage.template = kiwi.Vue.extend({template: null});
+        newMessage.template = kiwi.Vue.extend({ template: null });
       }
       captionTimer[timerKey] = Date.now();
-      // if this is a channel join (not a PM), then just display the nick. Otherwise show the message below.
+      // if this is a channel join (not a PM), then just display the nick.
+      // Otherwise show the message below.
       if (buffer.isChannel()) {
         message = '';
       } else {
         message = ' is inviting you to a private call.';
       }
       captions[timerKey].push(nick + message);
-      // only inject a new vue component if this is the first join message in groupedNoticesTTL milliseconds
-      if(captions[timerKey].length === 1) {
+      // only inject a new vue component if this is the first
+      // join message in groupedNoticesTTL milliseconds
+      if (captions[timerKey].length === 1) {
         messageTemplate.template = joinCallMessageComponent.extend({
           data() {
             return {
-              captions: captions[timerKey]
+              captions: captions[timerKey],
             };
           },
         });
       }
     }
   });
-  
-  function showCams(){
+
+  function showCams() {
     kiwi.emit('mediaviewer.show', { iframe: true, url: 'about:blank' });
     // Give some time for the mediaviewer to show up in the DOM
     setTimeout(loadJitsi, 10);
@@ -127,13 +131,12 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
     let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
     let jitsiBody = innerDoc.getElementsByTagName('body')[0];
     let innerHead = innerDoc.getElementsByTagName('head')[0];
-    
     let network = window.kiwi.state.getActiveNetwork();
     let buffer = window.kiwi.state.getActiveBuffer();
 
     let roomName = '';
     let m = null;
-    if (buffer.isQuery()){ // cam is being invoked in PM, not a channel
+    if (buffer.isQuery()) { // cam is being invoked in PM, not a channel
       let nicks = [];
       nicks.push(network.nick);
       nicks.push(buffer.name);
@@ -141,7 +144,7 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
       nicks[0] = 'query-' + nicks[0] + '#';
       roomName = nicks.join('');
       m = new network.ircClient.Message('PRIVMSG', buffer.name, '* ' + network.nick + ' is inviting you to a private call.');
-    }else{
+    } else {
       roomName = buffer.name;
       m = new network.ircClient.Message('PRIVMSG', buffer.name, '* ' + network.nick + ' has joined the conference.');
     }
@@ -150,33 +153,33 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
     network.ircClient.raw(m);
 
     let options = {
-        roomName: encodeRoomName(network.connection.server, roomName),
-        displayName: buffer.name,
-        parentNode: jitsiBody,
-        interfaceConfigOverwrite,
-        configOverwrite
-    }
-  
+      roomName: encodeRoomName(network.connection.server, roomName),
+      displayName: buffer.name,
+      parentNode: jitsiBody,
+      interfaceConfigOverwrite,
+      configOverwrite,
+    };
+
     if (kiwi.state.setting('conference.secure')) {
       // Get the JWT token from the network
-      kiwi.once('irc.raw.EXTJWT', function(command, message) {
-        token = message.params[1]
+      kiwi.once('irc.raw.EXTJWT', (command, message) => {
+        token = message.params[1];
         loadJitsiScript();
       });
       network.ircClient.raw('EXTJWT ' + roomName);
     } else {
       loadJitsiScript();
     }
-    
-    function loadJitsiScript () {
+
+    function loadJitsiScript() {
       // Load the jitsi script into the mediaviewer iframe
-      let jitsiAPIScript = innerDoc.createElement("script");
-      jitsiAPIScript.setAttribute("type", "text/javascript");
-      jitsiAPIScript.setAttribute("src", jitsiApiUrl);
-      jitsiAPIScript.addEventListener("load", function(event){
-        if(event.target.nodeName === "SCRIPT"){
-          jitsiBody.innerHTML="";
-          if(kiwi.state.setting('conference.secure')) {
+      let jitsiAPIScript = innerDoc.createElement('script');
+      jitsiAPIScript.setAttribute('type', 'text/javascript');
+      jitsiAPIScript.setAttribute('src', jitsiApiUrl);
+      jitsiAPIScript.addEventListener('load', (event) => {
+        if (event.target.nodeName === 'SCRIPT') {
+          jitsiBody.innerHTML = '';
+          if (kiwi.state.setting('conference.secure')) {
             options.jwt = token;
             options.noSsl = false;
           }
@@ -196,7 +199,6 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
     let mediaviewer = document.querySelector('.kiwi-mediaviewer');
     let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
     let jitsiDiv = innerDoc.getElementsByTagName('body')[0];
-    let innerHead = innerDoc.getElementsByTagName('head')[0];
 
     jitsiDiv.style.margin = 0;
     iframe.style.width = '100%';
@@ -206,7 +208,7 @@ kiwi.plugin('conferencePlugin', (kiwi, log) => {
     return iframe;
   }
 
-  function hideCams(){
+  function hideCams() {
     api.dispose();
     api = null;
     kiwi.emit('mediaviewer.hide');
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
+/* eslint-disable vue/html-indent */
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './plugin.js',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
-        ]
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-    }
+  mode: 'production',
+  entry: './plugin.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+  },
 };
